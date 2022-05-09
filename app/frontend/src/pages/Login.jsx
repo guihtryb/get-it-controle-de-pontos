6b9c1a62f87404089d91e1b45b332c928f29d070
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-09 00:32:48 -0300

    style: Tela Login - adiciona estilo main

--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -1,9 +1,16 @@
 import Header from "../components/Header";
+import LoginImage from "../components/LoginImage";
+import LoginTyping from "../components/LoginTyping";
+import '../styles/pages/Login.css'
 
 const Login = () => {
   return (
     <>
       <Header />
+      <main data-testid="login-main">
+        <LoginTyping />
+        <LoginImage />
+      </main>
     </>
   );
 };
