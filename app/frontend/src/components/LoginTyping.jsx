45025e217e4a870e0eb4ad8ad8611043cafdf464
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-09 00:24:31 -0300

    feat: Adiciona componentes LoginImage e LoginTyping e cumprindo testes

--- app/frontend/src/components/LoginTyping.jsx
+++ app/frontend/src/components/LoginTyping.jsx
@@ -1,3 +1,11 @@
-const LoginTyping = () => {};
+import '../styles/components/LoginTyping.css'
+
+const LoginTyping = () => {
+  return (
+    <span data-testid="typing-span" className="typing-span">
+      Troque pontos por produtos exclusivos!
+    </span>
+  );
+};
 
 export default LoginTyping;
