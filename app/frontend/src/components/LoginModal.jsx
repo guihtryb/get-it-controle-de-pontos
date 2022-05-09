4dc1ef2d4ac35b2125adeab17493b7d8b2b1e793
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-09 10:14:59 -0300

    feat: Tela Login - componentiza modal de login

--- app/frontend/src/components/LoginModal.jsx
+++ app/frontend/src/components/LoginModal.jsx
@@ -1,3 +1,29 @@
-const LoginModal = () => {};
+const LoginModal = () => {
+  return (
+    <section className="login-modal-container">
+    <div data-testid="login-modal" className="login-modal">
+      <form>
+        <label htmlFor="login-email-input">
+          Email:
+          <input
+            data-testid="login-email-input"
+            id="login-email-input"
+            type="email"
+          />
+        </label>
+        <label htmlFor="login-password-input">
+          Senha:
+          <input
+          data-testid="login-password-input"
+          id="login-password-input"
+          type="password"
+        />
+        </label>
+        <button data-testid="login-submit-btn" type="submit">submit</button>
+      </form>
+    </div>
+  </section>
+  );
+};
 
 export default LoginModal;
--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LoginImage from "../components/LoginImage";
+import LoginModal from "../components/LoginModal";
 import LoginTyping from "../components/LoginTyping";
 import '../styles/pages/Login.css'
 
@@ -11,19 +12,7 @@ const Login = () => {
         <LoginTyping />
         <LoginImage />
       </main>
-      <div data-testid="login-modal">
-      <form>
-        <label htmlFor="login-email-input">
-          Email:
-          <input  data-testid="login-email-input" id="login-email-input" type="email" />
-        </label>
-        <label htmlFor="login-password-input">
-          Senha:
-          <input  data-testid="login-password-input" id="login-password-input" type="password" />
-        </label>
-        <button data-testid="login-submit-btn" type="submit">submit</button>
-      </form>
-      </div>
+      <LoginModal />
     </>
   );
 };
