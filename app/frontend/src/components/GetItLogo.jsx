9e965d0bd4253b6a33b467e4d11a1823356db1ff
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-08 22:33:50 -0300

    feat: Tela Login - cria componente para logo

---
+++ app/frontend/src/components/GetItLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/components/GetItLogo.css';
+
+export const GetItLogo = () => {
+  return (
+    <span data-testid="get-it-logo" className="get-it-logo">
+      get it!
+    </span>
+  );
+};
