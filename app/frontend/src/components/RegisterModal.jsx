fb5b382958b6aa76c73c95344f90774b9071793a
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-09 23:43:52 -0300

    refactor: adiciona estado para valores do forms de registro e simulação inicial de cadastro

--- app/frontend/src/components/RegisterModal.jsx
+++ app/frontend/src/components/RegisterModal.jsx
@@ -1,16 +1,59 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Context from '../context/Context';
 import { handleShowNewModalClick, setDisplay } from '../utilis';
+import CloseButton from './CloseButton';
 
 import '../styles/components/RegisterModal.css';
-import CloseButton from './CloseButton';
+import { Redirect } from 'react-router-dom';
+
+
 
 const RegisterModal = () => {
   const {
     setShowRegisterModal,
     showRegisterModal,
-    setShowLoginModal
+    setShowLoginModal,
   } = useContext(Context);
+
+  const [fullName, setFullName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegistered, setIsRegistered] = useState(false);
+
+  const registerNewUser = async (rota, data) => {
+    console.log(
+      `usuario registrado com método POST na rota ${rota}`,
+      `--------------------------------------------------`,
+      `Informações de novo usuário:
+        Nome Completo: ${data.fullName}
+        username: ${data.username}
+        Email: ${data.email}
+      `
+    );
+  }
+  
+  const register = async (e) => {
+    e.preventDefault();
+
+    try {
+    const newUserData = {
+      fullName,
+      username,
+      email,
+      password
+    }
+  
+    const newUser /* { token, newUserData } */ = await registerNewUser('/users', newUserData);
+
+    localStorage.setItem('user', newUser);
+    setIsRegistered(true);
+    } catch (error) {
+      setIsRegistered(false);
+    }
+  };
+
+  if (isRegistered) return <Redirect to="/products" />
 
   return (
     <section
@@ -31,6 +74,8 @@ const RegisterModal = () => {
             data-testid="register-fullname-input"
             id="register-fullname-input"
             type="text"
+            value={fullName}
+            onChange={({ target: {value }}) => setFullName(value)}
           />
         </label>
         <label htmlFor="register-fullname-input">
@@ -39,6 +84,8 @@ const RegisterModal = () => {
             data-testid="register-username-input"
             id="register-username-input"
             type="text"
+            value={username}
+            onChange={({ target: {value }}) => setUsername(value)}
           />
         </label>
         <label htmlFor="register-email-input">
@@ -47,6 +94,8 @@ const RegisterModal = () => {
             data-testid="register-email-input"
             id="register-email-input"
             type="email"
+            value={email}
+            onChange={({ target: {value }}) => setEmail(value)}
           />
         </label>
         <label htmlFor="register-password-input">
@@ -55,9 +104,15 @@ const RegisterModal = () => {
           data-testid="register-password-input"
           id="register-password-input"
           type="password"
+          value={password}
+          onChange={({ target: { value }}) => setPassword(value)}
         />
         </label>
-        <button data-testid="register-submit-btn" type="submit">
+        <button
+          data-testid="register-submit-btn"
+          type="submit"
+          onClick={ (e) => register(e)}
+        >
           Registrar
         </button>
       </form>
