fd8db8eb1cbffa380650e05abe83882f94248cfd
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-09 22:53:45 -0300

    Adiciona Provider para a aplicação

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,12 +1,17 @@
 import './styles/App.css';
 import Login from './pages/Login';
 import { Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import Provider from './context/Provider';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={ Login } />
-    </Switch>
+    <Provider>
+      <Switch>
+        <Redirect exact from="/" to="/login" />
+        <Route exact path='/login' component={ Login } />
+      </Switch>
+    </Provider>
   );
 }
 
