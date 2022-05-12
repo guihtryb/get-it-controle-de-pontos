7287a855199c3845e1033306f83736506bcc4510
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-12 17:27:04 -0300

    feat: adiciona integração real com API para o método POST /login

---
+++ app/frontend/src/services/errorMessages.js
@@ -0,0 +1,6 @@
+const errorMessages = { 
+  400: 'Todos os campos precisam ser preenchidos! Por favor tente novamente.',
+  401: 'Senha ou email incorretos!  Por favor tente novamente.',
+};
+
+export default errorMessages;
