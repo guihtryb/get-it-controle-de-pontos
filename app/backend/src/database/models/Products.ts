0f964398d7a84b2440a0389741adf1b79799eb11
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-12 00:30:22 -0300

    feat: Cria model para Products

---
+++ app/backend/src/database/models/Products.ts
@@ -0,0 +1,53 @@
+import { BOOLEAN } from 'sequelize';
+import { Model, INTEGER, STRING, DECIMAL } from 'sequelize';
+import db from '.';
+
+class Products extends Model {
+  id!: number;
+  title!: string;
+  price!: number;
+  pricePoints!: number;
+  image!: string;
+  orderId!: number;
+  selled!: boolean;
+}
+
+Products.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: STRING(30),
+    allowNull: false,
+  },
+  price: {
+    type: DECIMAL(10,2),
+    allowNull: false,
+  },
+  pricePoints: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  image: {
+    type: STRING,
+    allowNull: false,
+  },
+  orderId: {
+    type: INTEGER,
+    allowNull: true,
+  },
+  selled: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'products',
+  timestamps: false
+});
+
+export default Products;
