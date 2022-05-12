be5c6e67b4c79beb897acad3a0a02ed8ed42d49d
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-12 10:50:23 -0300

    feat: adiciona funções jwt para criação e validação de token

---
+++ app/backend/src/jwt/index.ts
@@ -0,0 +1,21 @@
+import * as jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import { IUserTokenData } from '../interfaces';
+
+const secret = process.env.SECRET as string | 'GET_IT';
+
+const createToken = (userData: IUserTokenData) => jwt.sign(
+  { data: userData },
+  secret,
+  {
+    expiresIn: '5d',
+    algorithm: 'HS256',
+  },
+);
+
+const verifyToken = (token: string) => jwt.verify(token, secret);
+
+export {
+  createToken,
+  verifyToken,
+};
