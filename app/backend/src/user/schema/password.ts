6d7212d480c1564c94e71241feb7e0ecf6356c0c
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-12 19:32:37 -0300

    feat: adiciona schema para campo password

---
+++ app/backend/src/user/schema/password.ts
@@ -0,0 +1,28 @@
+import { StatusCode, ErrorMessage } from '../../enums';
+import { SchemaResponse } from '../../interfaces/';
+
+import {
+  invalidInputMinLength,
+  invalidInputType,
+  inputNotFilled,
+} from '../../validations';
+
+const passwordSchema = (password = ''): SchemaResponse[] => [
+  {
+    invalid: inputNotFilled(password),
+    code: StatusCode.BAD_REQUEST,
+    message: ErrorMessage.BAD_REQUEST,
+  },
+  {
+    invalid: invalidInputType(password, 'string'),
+    code: StatusCode.UNAUTHORIZED,
+    message: ErrorMessage.UNAUTHORIZED,
+  },
+  {
+    invalid: invalidInputMinLength(password, 7),
+    code: StatusCode.UNAUTHORIZED,
+    message: ErrorMessage.UNAUTHORIZED,
+  },
+];
+
+export default passwordSchema;
