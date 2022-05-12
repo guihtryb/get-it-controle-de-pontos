b32bef337dc8b6aff1c62c224df8bae12968fcb4
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-12 19:30:58 -0300

    feat: adiciona schema para campo email

---
+++ app/backend/src/user/schema/email.ts
@@ -0,0 +1,18 @@
+import { StatusCode, ErrorMessage } from '../../enums';
+import { SchemaResponse } from '../../interfaces';
+import { invalidEmail, inputNotFilled } from '../../validations';
+
+const emailSchema = (email = ''): SchemaResponse[] => [
+  {
+    invalid: inputNotFilled(email),
+    code: StatusCode.BAD_REQUEST,
+    message: ErrorMessage.BAD_REQUEST,
+  },
+  {
+    invalid: invalidEmail(email),
+    code: StatusCode.UNAUTHORIZED,
+    message: ErrorMessage.UNAUTHORIZED,
+  },
+];
+
+export default emailSchema;
---
+++ app/backend/src/user/schema/index.ts
@@ -0,0 +1,9 @@
+import emailSchema from './email';
+import fullNameSchema from './fullName';
+import usernameSchema from './username';
+
+export {
+  emailSchema,
+  fullNameSchema,
+  usernameSchema,
+};
