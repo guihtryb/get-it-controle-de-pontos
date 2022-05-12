50af2bd47ddf36239027a47b0256a4bdb8931137
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-12 17:58:12 -0300

    test: cobertura para caso de dados invalidos ao requisitar login em POST /login

--- app/backend/src/tests/login/login.test.ts
+++ app/backend/src/tests/login/login.test.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 import * as bcrypt from '../../bcrypt';
 import Users from '../../database/models/Users'
 import { Response } from 'superagent';
-import { StatusCode } from '../../enums';
+import { ErrorMessage, StatusCode } from '../../enums';
 import { user } from './mocks/user';
 
 chai.use(chaiHttp);
@@ -44,6 +44,54 @@ describe('Testando rota POST /login', () => {
 
       userCorrectFields.forEach((correctField) => {
         expect(chaiHttpResponse.body.user).to.have.property(correctField);
+      });
+    });
+  });
+  describe('Ao receber um inválido', () => {
+    const errorMessage = "Senha ou E-mail incorretos!";
+
+    describe('email', () => {
+      it('retorna o status 401 com uma mensagem de erro no body' , async () => {
+        sinon.stub(Users, "findOne")
+        .resolves(null);
+
+        sinon.stub(bcrypt, "compare")
+        .resolves(false);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: 'invalid@test', password: user.password });
+
+        expect(chaiHttpResponse).to.have.status(StatusCode.UNAUTHORIZED);
+
+        expect(chaiHttpResponse.body).to.be.an('object');
+        expect(chaiHttpResponse.body).to.have.property('message');
+
+        expect(chaiHttpResponse.body.message).to.be.a('string');
+        expect(chaiHttpResponse.body.message).to.be.equal(errorMessage);
+      });
+    });
+    describe('password', () => {
+      it('retorna o status 401 com uma mensagem de erro no body' , async () => {
+        sinon.stub(Users, "findOne")
+        .resolves(null);
+
+        sinon.stub(bcrypt, "compare")
+        .resolves(false);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: user.email, password: '' });
+
+        expect(chaiHttpResponse).to.have.status(StatusCode.BAD_REQUEST);
+
+        expect(chaiHttpResponse.body).to.be.an('object');
+        expect(chaiHttpResponse.body).to.have.property('message');
+
+        expect(chaiHttpResponse.body.message).to.be.a('string');
+        expect(chaiHttpResponse.body.message).to.be.equal(ErrorMessage.BAD_REQUEST);
       });
     });
   });
