cbfd4fd60e58d7d37c614b3744b4463fe7f3a403
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-13 18:52:40 -0300

    feat: adiciona componetes para o Header de ProductsUserView

--- app/frontend/src/pages/ProductsUserView.jsx
+++ app/frontend/src/pages/ProductsUserView.jsx
@@ -1,11 +1,17 @@
 import GetItLogo from "../components/GetItLogo";
 import Header from "../components/Header";
+import LogOutButton from "../components/LogOutButton";
+import Points from "../components/Points";
+import Username from "../components/Username";
 
 const ProductsUserView = () => {
   return (
     <>
       <Header
         getItLogo={<GetItLogo key="getItLogo"/>}
+        username={<Username key="username" />}
+        points={<Points key="points"/>}
+        logOutButton={<LogOutButton key="logOutButton"/>}
       />
     </>
   );
