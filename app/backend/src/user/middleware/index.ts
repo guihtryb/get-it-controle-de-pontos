fad091ed10da78ddf9c0e65d280695234959157d
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-13 11:03:11 -0300

    feat: adiciona camada de middlewares de POST /user

--- app/backend/src/user/middleware/index.ts
+++ app/backend/src/user/middleware/index.ts
@@ -1,34 +1,5 @@
-import { NextFunction, Request, Response } from 'express';
-import { fullNameSchema, usernameSchema, emailSchema, passwordSchema } from '../schema';
+import userPostMiddleware from "./post";
 
-const registerMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const { fullName, username, email, password } = req.body;
-
-  const fullNameInputValidations = fullNameSchema(fullName);
-
-  const usernameInputValidations = usernameSchema(username);
-
-  const emailInputValidations = emailSchema(email);
-
-  const passwordInputValidations = passwordSchema(password);
-
-  const invalidData = [
-    ...fullNameInputValidations,
-    ...usernameInputValidations,
-    ...emailInputValidations,
-    ...passwordInputValidations,
-  ].find(({ invalid }) => invalid);
-
-  if (invalidData) {
-    const { code, message } = invalidData;
-
-    return res.status(code).json({ message });
-  }
-  next();
+export {
+  userPostMiddleware,
 };
-
-export default registerMiddleware;
