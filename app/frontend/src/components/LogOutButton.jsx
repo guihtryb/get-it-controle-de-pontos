f01dc1e704d35b312aaa79a04461429c3b7a54b3
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-13 18:50:59 -0300

    feat: adiciona componente LogOutButton

--- app/frontend/src/components/LogOutButton.jsx
+++ app/frontend/src/components/LogOutButton.jsx
@@ -1,3 +1,23 @@
-const LogOutButton = () => {};
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import '../styles/components/LogOutButton.css'
+
+const LogOutButton = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/login');
+  }
+
+  return (
+    <button
+    data-testid="logout-btn"
+    className="logout-btn"
+    onClick={ handleClick }
+    >
+      Sair
+    </button>
+  );
+};
 
 export default LogOutButton;
