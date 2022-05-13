a37dab3dbb382acb41ed3510bcda5c6c1d12e040
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-13 11:07:39 -0300

    feat: método hash de bcrypt*

--- app/backend/src/bcrypt/index.ts
+++ app/backend/src/bcrypt/index.ts
@@ -4,3 +4,14 @@ export const compare = async (
   inputPassword:string,
   dbPassword: string
 ) => await bcrypt.compare(inputPassword, dbPassword);
+
+export const hash = async (
+  inputPassword:string,
+  saltRounds: number
+) => {
+  const salt = await bcrypt.genSalt(saltRounds);
+
+  const hashedPass = await bcrypt.hash(inputPassword, salt);
+
+  return hashedPass;
+};
