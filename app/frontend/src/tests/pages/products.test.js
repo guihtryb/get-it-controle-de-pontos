8f506908e15e4be0a2977d2525b7451da946f61c
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-13 15:59:36 -0300

    test: adiciona testes para página ProductsUserView

--- app/frontend/src/tests/pages/products.test.js
+++ app/frontend/src/tests/pages/products.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../helper/renderWithRouter';
 import ProductsUserView from '../../pages/ProductsUserView';
+import userEvent from '@testing-library/user-event';
 
 
 describe('Testando página ProductsUserView', () => {
@@ -11,5 +12,40 @@ describe('Testando página ProductsUserView', () => {
 
       expect(screen.getByTestId('get-it-header')).toBeInTheDocument();
     });
+    it('possui um elemento com o atributo data-testid igual a `get-it-logo`', () => {
+      renderWithRouter(<ProductsUserView />);
+
+
+      expect(screen.getByTestId('get-it-logo')).toBeInTheDocument();
+    });
+    it('possui um elemento com o atributo data-testid igual a `username-span` com o nome de usuário', () => {
+      renderWithRouter(<ProductsUserView />);
+
+      expect(screen.getByTestId('username-span')).toBeInTheDocument();
+    });
+    it('possui um elemento com o atributo data-testid igual a `user-points` com os pontos do usuário', () => {
+      renderWithRouter(<ProductsUserView />);
+
+      expect(screen.getByTestId(`user-points`)).toBeInTheDocument();
+    });
+    it('possui um botão com o atributo data-testid=`logout-btn` e texto igual a `Sair`', () => {
+      renderWithRouter(<ProductsUserView />);
+
+      expect(screen.getByTestId(`logout-btn`)).toBeInTheDocument();
+      expect(screen.getByTestId(`logout-btn`)).toHaveTextContent('Sair');
+    });
+    describe('Ao clicar no botão de data-testid igual a `logout-btn', () => {
+      it('O usuário retorna para a página de Login', () => {
+       const { history } = renderWithRouter(<ProductsUserView />);
+      
+      const logOutBtn = screen.getByTestId(`logout-btn`);
+
+      userEvent.click(logOutBtn);
+
+      const { pathname } = history.location;
+      
+      expect(pathname).toBe('/login');
+      });
+    })
   });
 });
