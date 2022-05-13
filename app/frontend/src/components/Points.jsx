56ab878821116b89081b3d3e95a59704034bb6a4
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-13 17:34:45 -0300

    feat: adiciona componente Points

--- app/frontend/src/components/Points.jsx
+++ app/frontend/src/components/Points.jsx
@@ -1,3 +1,15 @@
-const Points = () => {};
+import React from 'react';
+import '../styles/components/Points.css'
+
+const Points = () => {
+  return (
+    <div
+      className="user-points-container"
+    >
+      <span>Seus pontos:</span>
+      <span data-testid="user-points">pontos</span>
+    </div>
+  );
+};
 
 export default Points;
