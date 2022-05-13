814fd0b1e30edf712e21053cf65b3bec57fb4bc6
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-13 16:42:28 -0300

    feat: adiciona componente Username

--- app/frontend/src/components/Username.jsx
+++ app/frontend/src/components/Username.jsx
@@ -1,3 +1,18 @@
-const Username = () => {};
+import React from 'react';
+import '../styles/components/Username.css'
+import { getUsername } from '../utilis';
+
+
+const Username = () => {
+
+  return (
+    <span 
+      data-testid="username-span"
+      className="username-span"
+    >
+      {`Hi, ${ getUsername() }!`}
+    </span>
+  );
+};
 
 export default Username;
