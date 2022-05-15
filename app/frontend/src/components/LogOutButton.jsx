dae7b565634693e0b6c029dfbb6d9ecfd0c30e2d
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-15 13:09:07 -0300

    update: atualiza função de logout

--- app/frontend/src/components/LogOutButton.jsx
+++ app/frontend/src/components/LogOutButton.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import '../styles/components/LogOutButton.css'
 
-const LogOutButton = () => {
+import '../styles/components/LogOutButton.css';
+
+function LogOutButton() {
   const history = useHistory();
 
   const handleClick = () => {
+    localStorage.clear();
+
     history.push('/login');
-  }
+  };
 
   return (
     <button
-    data-testid="logout-btn"
-    className="logout-btn"
-    onClick={ handleClick }
+      type="button"
+      data-testid="logout-btn"
+      className="logout-btn"
+      onClick={handleClick}
     >
       Sair
     </button>
   );
-};
+}
 
 export default LogOutButton;
