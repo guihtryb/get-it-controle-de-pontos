5ce086505d8ed5ad3d2c8bbcd317298a6c6743e7
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-15 12:55:31 -0300

    feat: adiciona component EditProductButton

--- app/frontend/src/components/EditProductButton.jsx
+++ app/frontend/src/components/EditProductButton.jsx
@@ -1,3 +1,22 @@
-const EditProductButton = () => {};
+import PropTypes from 'prop-types';
+import React from 'react';
+import '../styles/components/EditProductButton.css';
+import editBtn from '../images/icons8-edit-33.png';
+
+function EditProductButton({ index }) {
+  return (
+    <button
+      data-testid={`edit-product-${index}`}
+      className="edit-btn"
+      type="button"
+    >
+      <img src={editBtn} alt="edit button" />
+    </button>
+  );
+}
+
+EditProductButton.propTypes = {
+  index: PropTypes.number.isRequired,
+};
 
 export default EditProductButton;
