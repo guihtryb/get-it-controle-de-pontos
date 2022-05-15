117d47988a26506a3af8d57cb23a08c6eff3999b
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-15 16:30:22 -0300

    update: atualiza provider

--- app/frontend/src/context/Provider.jsx
+++ app/frontend/src/context/Provider.jsx
@@ -1,20 +1,43 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import Context from './Context';
+import products from '../mocks/products';
 
 export default function Provider({ children }) {
   const [showLoginModal, setShowLoginModal] = useState(false);
-  const [showRegisterModal, setShowRegisterModal] = useState(false);
+  const [showRegisterUserModal, setShowRegisterUserModal] = useState(false);
+  const [showRegisterProductModal, setShowRegisterProductModal] = useState(false);
+  const [showDeleteProductModal, setShowDeleteProductModal] = useState(false);
+  const [askedToDelete, setAskedToDelete] = useState();
+  const [showEditProductModal, setShowEditProductModal] = useState(false);
+  const [askedToEdit, setAskedToEdit] = useState();
 
   const contextValue = {
     setShowLoginModal,
     showLoginModal,
-    showRegisterModal,
-    setShowRegisterModal,
+    showRegisterUserModal,
+    setShowRegisterUserModal,
+    setShowRegisterProductModal,
+    showRegisterProductModal,
+    setShowDeleteProductModal,
+    showDeleteProductModal,
+    setAskedToDelete,
+    askedToDelete,
+    setShowEditProductModal,
+    showEditProductModal,
+    setAskedToEdit,
+    askedToEdit,
+    products,
   };
 
   return (
-    <Context.Provider value={ contextValue }>
+    <Context.Provider value={contextValue}>
       { children }
     </Context.Provider>
   );
 }
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
