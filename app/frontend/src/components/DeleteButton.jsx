5c79099ae3db7803f2a52013548849a5f319bb2b
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-15 12:57:59 -0300

    feat: adiciona componente DeleteButton

--- app/frontend/src/components/DeleteButton.jsx
+++ app/frontend/src/components/DeleteButton.jsx
@@ -1,3 +1,22 @@
-const DeleteButton = () => {};
+import PropTypes from 'prop-types';
+import React from 'react';
+import '../styles/components/DeleteProductButton.css';
+import deleteBtn from '../images/icons8-remove-38.png';
+
+function DeleteButton({ index }) {
+  return (
+    <button
+      data-testid={`delete-product-${index}`}
+      className="delete-btn"
+      type="button"
+    >
+      <img src={deleteBtn} alt="delete button" />
+    </button>
+  );
+}
+
+DeleteButton.propTypes = {
+  index: PropTypes.number.isRequired,
+};
 
 export default DeleteButton;
