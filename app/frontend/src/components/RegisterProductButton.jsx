94f807fc182106d4ec03c02874f6a027b52ba6ab
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-15 13:10:01 -0300

    update: adiciona lógica para desabilitar botão

--- app/frontend/src/components/RegisterProductButton.jsx
+++ app/frontend/src/components/RegisterProductButton.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import '../styles/components/RegisterProductButton.css'
+import React, { useContext } from 'react';
+import Context from '../context/Context';
+import '../styles/components/RegisterProductButton.css';
+import { getUserRole } from '../utilis/getUserRole';
 
-const RegisterProductButton = () => {
+function RegisterProductButton() {
+  const { setShowRegisterProductModal } = useContext(Context);
+  const isDisable = getUserRole() === 'user';
+
   return (
     <button
       data-testid="new-product-btn"
       className="new-product-btn"
+      onClick={() => setShowRegisterProductModal(true)}
+      type="button"
+      disabled={isDisable}
     >
       Cadastrar produto
     </button>
-  )
+  );
 }
 
-export default RegisterProductButton
+export default RegisterProductButton;
