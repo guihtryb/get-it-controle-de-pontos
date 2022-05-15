47893e2da1669d19a104c8a6f45763364e17ce52
Guilherme Viana <guilhermehviana01@gmail.com>
2022-05-15 13:06:51 -0300

    update: ProductsPageTitle recebe texto de acordo com tipo de usuario (admin/user)

--- app/frontend/src/components/ProductsPageTitle.jsx
+++ app/frontend/src/components/ProductsPageTitle.jsx
@@ -1,16 +1,22 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+
 import '../styles/components/ProductsPageTitle.css';
+import { getUserRole } from '../utilis';
 
-
-export const ProductsPageTitle = ({ text }) => {
+function ProductsPageTitle({ text }) {
+  const role = getUserRole();
   return (
     <h1
-      data-testid="products-userview-title"
-      className="products-userview-title"
+      className={`products-${role}view-title`}
     >
       { text }
     </h1>
   );
+}
+
+ProductsPageTitle.propTypes = {
+  text: PropTypes.string.isRequired,
 };
 
 export default ProductsPageTitle;
